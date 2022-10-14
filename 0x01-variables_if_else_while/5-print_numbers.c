2c4df6169c9e48df59dd422e4d7534c26282ea8a
abraham afework <abrahamafework7@gmail.com>
2022-10-14 15:00:09 -0700

    single digit till 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main -all the excution carried out here
+ * Return: returns 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+		printf("\n");
+
+	return (0);
+}
