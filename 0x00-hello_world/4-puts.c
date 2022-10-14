a2232a8fa4edd3dccf6227a696a1ae4b0e0f9587
abraham afework <abrahamafework7@gmail.com>
2022-10-14 02:45:10 -0700

    displying output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - This prints the text given in puts.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
