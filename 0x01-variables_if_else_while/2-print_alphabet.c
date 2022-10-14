abe407688a9029fb2114851425786453873638e8
abraham afework <abrahamafework7@gmail.com>
2022-10-14 11:46:54 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  * main - all things are excuited here
+  * Return: returns the value 0.
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet < 'z')
+	{
+	putchar(alphabet);
+	alphabet++;
+	}
+	return (0);
+}
