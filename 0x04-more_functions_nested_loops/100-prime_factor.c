55511bb22277e4335fdd31bf00b85db7da19687d
abraham afework <abrahamafework@gmail.com>
2022-10-22 01:57:51 +0300

    The prime factors of

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - print out largest prime factorial
+ *
+ * Description: it will print the largest prime factor of '612852475143
+ *
+ *Return: 0 always
+ *
+ */
+int main(void)
+{
+	long num;
+	int primeNum;
+
+	num = 612852475143;
+	primeNum = 2;
+
+	while (primeNum < num)
+	{
+		if (num % primeNum == 0)
+		{
+			num = num / primeNum;
+			primeNum = 2;
+		}
+		else
+			primeNum++;
+	}
+	printf("%lu\n", num);
+	return (0);
+
+}
