43786a281f554196aad143c429ae62075d7e22c9
abraham <Abrahamafework7@gmail.com>
2022-10-24 19:08:11 +0200

    inintial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+* reset_to_98 - function
+* Description - receives the address of n and convert to value to 98
+*@n: the address of n is passed
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef __RESET_TO_90_H
+#define
+void reset_to_98(int *n);
+
+#endif
