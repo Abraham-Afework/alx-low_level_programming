e5554d892359d7d96b073524bc7c673ac9da0e3c
abraham afework <abrhamafework7@gmail.com>
2022-10-25 20:42:08 +0300

    prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+* print_rev - outputs the reverse of a stirng
+*Description - receives the address of char and tries to reverse the string
+*@s: the address of the first char
+*/
+void print_rev(char *s)
+{
+	int count, j;
+
+	count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s = s + 1;
+	}
+	for (j = 0; j < count; j++)
+	{
+		_putchar(*(s - j));
+	}
+	_putchar('\n');
+}
