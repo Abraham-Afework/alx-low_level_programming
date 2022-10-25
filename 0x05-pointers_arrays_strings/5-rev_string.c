47dce020829863731c2c3d2168aebc16ced59e05
abraham afework <abrhamafework7@gmail.com>
2022-10-26 02:28:23 +0300

    array element print

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,28 @@
-#include"main.h"
+#include "main.h"
+
 /**
-* rev_string - outputs the reverse of a stirng
-*Description - receives the address of string and tries to reverse the string
-*@s: the address of the first character
-*/
+  * rev_string - reverse a string.
+  * @s: string to reverse
+  */
 void rev_string(char *s)
 {
-	int j, i;
-	int count;
-	char *reverse;
-	reverse = s;
-        count = 0;
+	int begin, temp;
+	int finish = 0;
 
-        while (*reverse != '\0')
-        {
-                count++;
-               reverse = reverse + 1;
-        }
-        
-	
-	for (j = count,i=0; j =0; j--)
+	while (*(s + finish) != '\0')
 	{
-		reverse[i] = *(s-j);
-		i++;
+		finish++;
+	}
+
+	finish -= 1;
+	begin = 0;
+	while (finish > begin)
+	{
+		temp = s[begin];
+		s[begin] = s[finish];
+		s[finish] = temp;
+		begin++;
+		finish--;
 	}
 
 }
