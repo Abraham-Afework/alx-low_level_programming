f864f61fdb669d6c428dab26e1c97a4e7ea9d273
Abraham Afework <Abrahamafework7@gmail.com>
2022-11-14 22:43:51 +0200

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+/**
+ * struct dog - contains detail information about dog
+ *
+ *
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
