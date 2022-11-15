a47dce02a2bc78a40abdc54adb6e3872cdbef563
Abraham Afework <Abrahamafework7@gmail.com>
2022-11-16 01:02:56 +0200

    type defination of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,21 +3,26 @@
 /**
  * struct dog - contains detail information about dog
  *
- *
+ * dog_t - typedef for struct dog
  * @name: dogs name
  * @age: dogs age
  * @owner: dogs owner
  *
  */
-struct dog
+typedef struct dog
 {
 
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
 #endif
