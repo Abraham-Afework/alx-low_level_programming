9a6d498a74097ec0ad3f51441a80265ed9f81dd4
Abraham Afework <Abrahamafework7@gmail.com>
2022-12-09 00:22:22 +0200

    file creation:

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include"main.h"
+/**
+ * create_file - Create a new files
+ * @filename: Name of the file
+ * @text_content: Content of the file
+ * Return: Success is 1, and if failed is -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+        int fp,length, fwrite;
+	length = 0;
+
+        if (filename == NULL)
+                return (-1);
+
+        fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+        if (fp == -1)
+                return (-1);
+        if (text_content != NULL)
+        {
+		while (text_content[length] != '\0')
+		{
+			length++;
+		}
+		fwrite = write(fp, text_content, (length));
+		if (fwrite == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
+}
